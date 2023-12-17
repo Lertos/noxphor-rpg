04489dbb7b7a34179c1ecf403a0567fbd60f1b5f
Lertos <deetasticgaming@gmail.com>
2023-12-17 19:12:28 -0400

    Making sure popups are above most sprites

--- main.gd
+++ main.gd
@@ -5,7 +5,11 @@ const SCENE_INTERACT_OPTIONS = preload("res://scenes/ui/interact_options.tscn")
 func spawn_interact_popup(pos: Vector2, options: Array):
 	var popup = SCENE_INTERACT_OPTIONS.instantiate()
 	
+	if $Popups.get_child_count() > 0:
+		for child in $Popups.get_children():
+			child.queue_free()
+	
 	popup.position = pos
 	popup.show_options(options)
 	
-	add_child(popup)
+	$Popups.add_child(popup)
