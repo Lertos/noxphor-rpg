d0aa678dc974b23df60c1fe98473a8b24045652b
Lertos <deetasticgaming@gmail.com>
2023-12-18 17:33:52 -0400

    Asset cleanup. Adding movement via tree

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
+@onready var anim_tree: AnimationTree = $AnimationTree
+
 const SPEED: float = 100.0
+
+var input_vector: Vector2 = Vector2.ZERO
+
+func _process(delta):
+	update_anim_conditions()
 
 func _physics_process(delta):
 	if not States.is_state(States.STATE.PLAYING):
 		return
-	
-	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up")
@@ -15,16 +20,6 @@ func _physics_process(delta):
 	
 	if input_vector:
 		velocity = input_vector * SPEED
-		
-		#Very naive approach; should be replaced by a AnimTree once you have walk anims
-		if velocity.x > 0:
-			$Main.frame = 2
-		elif velocity.x < 0:
-			$Main.frame = 1
-		elif velocity.y > 0:
-			$Main.frame = 0
-		elif velocity.y < 0:
-			$Main.frame = 3
 	else:
 		velocity = input_vector
 		
@@ -38,3 +33,15 @@ func _input(event):
 		for area in $Area2D.get_overlapping_areas():
 			if area.has_method("interact"):
 				area.interact()
+
+func update_anim_conditions():
+	if velocity == Vector2.ZERO:
+		anim_tree["parameters/conditions/is_idle"] = true
+		anim_tree["parameters/conditions/is_moving"] = false
+	else:
+		anim_tree["parameters/conditions/is_idle"] = false
+		anim_tree["parameters/conditions/is_moving"] = true
+	
+	if input_vector != Vector2.ZERO:
+		anim_tree["parameters/Idle/blend_position"] = input_vector
+		anim_tree["parameters/Run/blend_position"] = input_vector
