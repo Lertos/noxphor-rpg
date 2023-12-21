31f465798a1f81da4146b3ae1dab071197b4478f
Lertos <deetasticgaming@gmail.com>
2023-12-20 20:51:13 -0400

    Adding scene changer

---
+++ scenes/objects/instance_changer.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+@export_file var scene_to_load: String
+
+#TODO: In the scene that is being changed to, you need to have some spawn points and 
+#then load the player into those spawn points based on the previous scene
+
+func on_body_entered(body):
+	if body.name == "Player":
+		if scene_to_load != null:
+			get_tree().get_root().get_node("root").change_maps(self.get_parent(), scene_to_load)
