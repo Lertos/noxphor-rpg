50e9eeaabbe7cc992ddafc8d94e4133b65e63c06
Lertos <deetasticgaming@gmail.com>
2024-01-01 11:28:27 -0400

    Adding new manager scripts

---
+++ scripts/character_manager.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name CharacterManager
+
+func _init():
+	print("CharacterManager loaded")
---
+++ scripts/dialogue_manager.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name DialogueManager
+
+func _init():
+	print("DialogueManager loaded")
