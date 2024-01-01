371cf6a7c963dd79d19e1f110e4148b2ec0bec06
Lertos <deetasticgaming@gmail.com>
2024-01-01 12:17:58 -0400

    Adding helper singleton

---
+++ global_scripts/helper.gd
@@ -0,0 +1,8 @@
+extends Node
+
+#An atrocious workaround... The fact RTL's can't just auto-adjust width is ridiculous
+func text_width(label):
+	var theme_font = label.get_theme_font("font")
+	var string_size = theme_font.get_string_size(label.text, HORIZONTAL_ALIGNMENT_LEFT, -1, label.get_theme_font_size("font_size"))
+	
+	return string_size.x
